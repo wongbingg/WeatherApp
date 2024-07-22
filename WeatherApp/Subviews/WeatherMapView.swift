adcf3bb2768a6844d1af20216e58518c0ca8f894
wongbingg <wb277@naver.com>
2024-07-22 17:33:02 +0900

    feat : 네번째 WeatherMapView 생성 #3
    - 강수량을 나타내는 MapView 구현

---
+++ WeatherApp/Subviews/WeatherMapView.swift
@@ -0,0 +1,62 @@
+//
+//  WeatherMapView.swift
+//  WeatherApp
+//
+//  Created by 이원빈 on 2024/07/22.
+//
+
+import UIKit
+import MapKit
+
+final class WeatherMapView: BaseView {
+    
+    // MARK: - UI
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "강수량"
+        return label
+    }()
+    private let mapView: MKMapView = {
+        let mapView = MKMapView(frame: .zero)
+        return mapView
+    }()
+    
+    // MARK: - Overrides
+    override func setupViewHierarchy() {
+        self.addSubview(descriptionLabel)
+        self.addSubview(mapView)
+    }
+    
+    override func setupViewConstraints() {
+        descriptionLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+        }
+        mapView.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    override func setupInitialSetting() {
+        mapView.delegate = self
+    }
+    
+    // MARK: - Methods
+    func addPinAndFocus(at latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        // 핀 추가
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "Destination"
+        mapView.addAnnotation(annotation)
+        
+        // 포커스 이동
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension WeatherMapView: MKMapViewDelegate {
+    
+}
